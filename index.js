36774ee7cc103accbd67e9efdcf11f901c5b548c
Pranjal Agrawal <pranjal605@gmail.com>
2022-03-21 00:50:05 +0530

    setup routes and controller for home page

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 const port = 8000;
 
 
+
+//use routers
+
+app.use('/', require('./routes/index'));
+
+
 app.listen(port, function(err){
     if(err){
         console.log("error surver is not running on port : ", port);
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const routes = express.Router();
+const homeController = require('../conrollers/home-controller');
+
+module.exports = routes;
+
+console.log("hii from router/indes.js");
+
+routes.get('/', homeController.home);
