c0e8c781bf41a29681817bbe942ba6547cca4923
Pranjal Agrawal <pranjal605@gmail.com>
2022-03-25 22:43:42 +0530

    25-03-22 commit

--- conrollers/home-controller.js
+++ conrollers/home-controller.js
@@ -1,3 +1,5 @@
 module.exports.home = function(req, res){
-    return res.end('<h1> express is up for codersConnect</h1>');
+    return res.render('home', {
+        title: "CodersConnect"
+    });
 }
--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const app = express();
 const port = 8000;
 
 
-
 //use routers
-
 app.use('/', require('./routes/index'));
+
+// setup the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 
 app.listen(port, function(err){
     if(err){
         console.log("error surver is not running on port : ", port);
     }
-
     console.log("surver is runing on port : ", port);
-
 })
