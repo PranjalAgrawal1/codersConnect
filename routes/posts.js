ff21725c11a6a49d6ae9a3fd9bb156fda0cbf00a
Pranjal Agrawal <pranjal605@gmail.com>
2022-04-06 21:34:36 +0530

    adding security -> so that user cannot post without identity

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const postController = require('../conrollers/post-controller');
 
 
-router.post('/create', postController.create);
+router.post('/create', passport.authenticate, postController.create);
 
 
 module.exports = router;
